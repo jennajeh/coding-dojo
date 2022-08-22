82a75819abfeee20c9e693a0c3b59eea4b012ddd
jennajeh <jennajuneh@gmail.com>
2022-08-22 09:31:28 +0900

    20220822-코딩도장

---
+++ Solution/java/20220822/src/Solution.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        for (int i = 0; i < b; i += 1) {
+            for (int j = 0; j < a; j += 1) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
