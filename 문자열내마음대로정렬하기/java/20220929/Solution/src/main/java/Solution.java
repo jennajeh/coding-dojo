88661be709904d7f01499f4d136ec3a0ff0feb08
jennajeh <jennajuneh@gmail.com>
2022-09-29 10:18:49 +0900

    20220929 코딩도장: 문자열 내 마음대로 정렬하기

---
+++ 문자열내마음대로정렬하기/java/20220929/Solution/src/main/java/Solution.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+    public String[] solution(String[] strings, int n) {
+
+        for (int i = 0; i < strings.length; i += 1) {
+            strings[i] = strings[i].charAt(n) + strings[i];
+        }
+
+        Arrays.sort(strings);
+
+        for (int i = 0; i < strings.length; i += 1) {
+            strings[i] = strings[i].substring(1);
+        }
+
+        return strings;
+    }
+}
