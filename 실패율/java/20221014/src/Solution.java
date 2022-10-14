56329946eb9874278d115738a8b32bac25244990
jennajeh <jennajuneh@gmail.com>
2022-10-14 10:22:56 +0900

    20221014 코딩 도장: 실패율

---
+++ 실패율/java/20221014/src/Solution.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    public int[] solution(int N, int[] stages) {
+        Map<Integer, Double> map = new HashMap<>();
+
+        //각 스테이지에 머물러 있는 플레이어 수
+        //스테이지 번호가 1~N까지이므로 0 인덱스는 버리기 위해 N+2
+        int[] userFailCounts = new int[N+2];
+        //각 스테이지에 도달한 플레이어 수
+        int[] userTotalCounts = new int[N+1];
+
+        //스테이지별 머물러 있는 플레이어 수 카운트
+        for (int stage : stages) {
+            userFailCounts[stage] += 1;
+        }
+
+        //스테이지 별 실패율 계산
+        for (int i = 1; i < userTotalCounts.length; i += 1) {
+            if (userFailCounts[i] == 0 || userTotalCounts[i] == 0) {
+                map.put(i, 0.0);
+                continue;
+            }
+
+            map.put(i, (double) (userFailCounts[i] / userTotalCounts[i]));
+        }
+
+        //실패율(value) 값으로 스테이지 번호(key)를 내림차순으로 정렬
+        List<Integer> list = new ArrayList<>(map.keySet());
+        Collections.sort(list, ((o1, o2) -> Double.compare(map.get(o2), map.get(01))));
+
+        return list.stream()
+                //Integer 에서 int 로 변환
+                .mapToInt(Integer::intValue).toArray();
+    }
+}
