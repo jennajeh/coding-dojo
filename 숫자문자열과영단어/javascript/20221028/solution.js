386e36ead15f6affdb8ff86e7fd515684cdeed3f
jennajeh <jennajuneh@gmail.com>
2022-10-28 14:03:59 +0900

    javascript 풀이

---
+++ 숫자문자열과영단어/javascript/20221028/solution.js
@@ -0,0 +1,17 @@
+const numbers = {
+  zero: '0',
+  one: '1',
+  two: '2',
+  three: '3',
+  four: '4',
+  five: '5',
+  six: '6',
+  seven: '7',
+  eight: '8',
+  nine: '9',
+};
+
+export default function replace(s) {
+  return parseInt(Object.keys(numbers)
+    .reduce((acc, key) => acc.replaceAll(key, numbers[key]), s), 10);
+}
