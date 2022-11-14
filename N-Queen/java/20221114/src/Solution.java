8e265952700deab1625a274ace789175ea78da9b
jennajeh <jennajuneh@gmail.com>
2022-11-14 11:25:55 +0900

    README 추가, 테스트코드 추가

--- N-Queen/java/20221114/src/Solution.java
+++ N-Queen/java/20221114/src/Solution.java
@@ -1,18 +1,18 @@
 import java.util.*;
+
 class Solution {
     int[] board;
     int count = 0;
 
     public int solution (int n) {
-
         board = new int[n];
-        backTracking(n, 0);
+
+        foundQueenRoute(n, 0);
 
         return count;
     }
 
-    public void backTracking (int n, int row){
-
+    public void foundQueenRoute (int n, int row){
         if(row == n){
             count += 1;
             return;
@@ -21,15 +21,13 @@ class Solution {
         for(int i = 0; i < n; i += 1){
             board[row] = i;
             if (possible(row)) {
-                backTracking(n, row + 1);
+                foundQueenRoute(n, row + 1);
             }
         }
-
     }
 
     public boolean possible(int row){
-
-        for(int i=0; i < row; i += 1){
+        for(int i = 0; i < row; i += 1){
             if(board[i] == board[row]){
                 return false;
             }
