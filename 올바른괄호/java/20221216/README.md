6e9fc45e00dc9dee30fce9f6377e7acea78d8fe6
jennajeh <jennajuneh@gmail.com>
2022-12-16 12:43:57 +0900

    20221216 코딩도장: 올바른 괄호

---
+++ 올바른괄호/java/20221216/README.md
@@ -0,0 +1,19 @@
+### 문제
+
+프로그래머스 [올바른 괄호](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=java)
+
+#### 1. 이해
+
+- 처음이 ')' 로 시작하면 무조건 false 이다.
+
+#### 2. 계획
+
+- 만약 '(' 라면 stack에 쌓고, ')' 라면 stack에서 pop을 해서 stack과 짝이 맞는지 확인한다.
+
+#### 3. 실행
+
+- ..
+
+#### 4. 반성
+
+- ..
---
+++ 올바른괄호/java/20221216/src/Solution.java
@@ -0,0 +1,39 @@
+import java.util.Stack;
+
+public class Solution {
+    boolean solution(String s) {
+        boolean answer = findTrueOrFalse(s);
+
+        return answer;
+    }
+
+    public boolean findTrueOrFalse(String s) {
+        boolean answer = true;
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
+
+                continue;
+            }
+
+            if (c == ')') {
+                if (stack.isEmpty()) {
+                    answer = false;
+
+                    break;
+                }
+
+                stack.pop();
+            }
+        }
+
+        if (!stack.isEmpty()) {
+            answer = false;
+        }
+
+        return answer;
+    }
+}
