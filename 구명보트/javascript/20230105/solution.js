4595cdd28945e446bc1d667c8055bdc7e6bcbebe
jennajeh <jennajuneh@gmail.com>
2023-01-05 09:49:25 +0900

    javascript 테스트코드 추가

--- 구명보트/javascript/20230105/solution.js
+++ 구명보트/javascript/20230105/solution.js
@@ -1,5 +1,13 @@
+function test() {
+  console.log('solution([70, 50, 80, 50], 100) : 3 --> ' + solution([70, 50, 80, 50], 100));
+  console.log('solution([70, 80, 50], 100) : 3 --> ' + solution([70, 80, 50], 100));
+  console.log('solution([70, 50, 30, 50], 100) : 2 --> ' + solution([70, 50, 30, 50], 100));
+}
+
+//test();
+
 function solution(people, limit) {
-  const answer = 0;
+  let answer = 0;
 
   people.sort((a, b) => a - b);
 
