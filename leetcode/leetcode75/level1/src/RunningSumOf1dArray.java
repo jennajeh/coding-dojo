2a6fda24fd55be68d65100253d19007f7f0b355f
jennajeh <jennajuneh@gmail.com>
2023-02-14 18:44:40 +0900

    Intstream 이용한 풀이 (LC1480)

--- leetcode/leetcode75/level1/src/RunningSumOf1dArray.java
+++ leetcode/leetcode75/level1/src/RunningSumOf1dArray.java
@@ -1,11 +1,22 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class RunningSumOf1dArray {
     public int[] runningSum(int[] nums) {
-        for (int i = 1; i < nums.length; i += 1) {
-            nums[i] = nums[i - 1] + nums[i];
-        }
+//        <for 문 이용한 풀이>
+//        for (int i = 1; i < nums.length; i += 1) {
+//            nums[i] = nums[i - 1] + nums[i];
+//        }
+//
+//        return nums;
+
+
+//      <stream 이용한 풀이>
+        IntStream.range(1, nums.length)
+                .forEach(i -> {
+                    nums[i] = nums[i - 1] + nums[i];
+                });
 
         return nums;
     }
